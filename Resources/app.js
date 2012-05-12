c98a08507bf1544e126035ac7d4d35437fb414b2
nosu <w.inoue@gmail.com>
2012-05-12 19:15:14 +0900

    mixin, combineを分離

--- Resources/app.js
+++ Resources/app.js
@@ -1,35 +1,5 @@
 (function() {
 	bz = {};
-
-	var empty = {};
-	function mixin(/*Object*/ target, /*Object*/ source){
-		var name, s, i;
-		for(name in source){
-			s = source[name];
-			if(!(name in target) || (target[name] !== s && (!(name in empty) || empty[name] !== s))){
-				target[name] = s;
-			}
-		}
-		return target; // Object
-	};
-	bz.mixin = function(/*Object*/ obj, /*Object...*/ props){
-		if(!obj){ obj = {}; }
-		for(var i=1, l=arguments.length; i<l; i++){
-			mixin(obj, arguments[i]);
-		}
-		return obj; // Object
-	};
-	
-	//create a new object, combining the properties of the passed objects with the last arguments having
-	//priority over the first ones
-	bz.combine = function(/*Object*/ obj, /*Object...*/ props) {
-		var newObj = {};
-		for(var i=0, l=arguments.length; i<l; i++){
-			mixin(newObj, arguments[i]);
-		}
-		return newObj;
-	};
-
 	bz.ui = {};
 	bz.ui.properties = {};
 
@@ -41,9 +11,8 @@
 	
 	Ti.include("ui/common/ui.js");
 	Ti.include("ui/common/styles.js");
-				
-	//considering tablet to have one dimension over 900px - this is imperfect, so you should feel free to decide
-	//yourself what you consider a tablet form factor for android
+	Ti.include("lib/mixin.js");
+	
 	var isTablet = bz.ui.properties.osname === 'ipad' || (bz.ui.properties.osname === 'android' && (bz.ui.properties.width > 899 || bz.ui.properties.height > 899));
 	
 	var Window;
---
+++ Resources/lib/mixin.js
@@ -0,0 +1,36 @@
+/**
+ * tweetanium（https://github.com/appcelerator-titans/tweetanium）,
+ * dojo（http://livedocs.dojotoolkit.org/dojo/_base/lang#mixin）
+ * を参照
+ */
+
+(function () {
+	var empty = {};
+	function mixin(/*Object*/ target, /*Object*/ source){
+		var name, s, i;
+		for(name in source){
+			s = source[name];
+			if(!(name in target) || (target[name] !== s && (!(name in empty) || empty[name] !== s))){
+				target[name] = s;
+			}
+		}
+		return target; // Object
+	};
+	bz.mixin = function(/*Object*/ obj, /*Object...*/ props){
+		if(!obj){ obj = {}; }
+		for(var i=1, l=arguments.length; i<l; i++){
+			mixin(obj, arguments[i]);
+		}
+		return obj; // Object
+	};
+	
+	//create a new object, combining the properties of the passed objects with the last arguments having
+	//priority over the first ones
+	bz.combine = function(/*Object*/ obj, /*Object...*/ props) {
+		var newObj = {};
+		for(var i=0, l=arguments.length; i<l; i++){
+			mixin(newObj, arguments[i]);
+		}
+		return newObj;
+	};
+}();
